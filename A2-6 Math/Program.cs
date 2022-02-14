35f568b792cc32ec49af120e24844365a9514ea3
GRADEN BISHOP <4400112995@OTECH.EDU>
2022-02-14 14:00:17 -0700

    first commit

---
+++ A2-6 Math/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A2_6_Math
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("I don't like to do math...");
+        }
+    }
+}
