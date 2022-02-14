3b01e92b0944f2198382ebc123b9547dd68441fe
GRADEN BISHOP <GRADEN.BISHOP2995@OMAIL.OTECH.EDU>
2022-02-14 14:29:01 -0700

    read pi

--- A2-6 Math/Program.cs
+++ A2-6 Math/Program.cs
@@ -6,7 +6,22 @@ namespace A2_6_Math
     {
         static void Main()
         {
-            Console.WriteLine("I don't like to do math...");
+            decimal diameter;
+            decimal circumference;
+            decimal area;
+            decimal rad;
+
+
+            Console.WriteLine("enter circle radius");
+            rad = decimal.Parse(Console.ReadLine());
+
+            diameter = rad * 2;
+            //must read section on pi and on decimals.
+            circumference = diameter * 3.14;
+
+
+            
+            Console.WriteLine($"diameter = {diameter}");
         }
     }
 }
